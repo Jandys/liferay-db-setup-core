8035b427915d01e446d5857862fba9eee47b08b7
Jakub Jandák <jakub.jandak@lundegaard.eu>
2023-02-13 10:42:24 +0100

    AREAJAVA-458 tests crated.

---
+++ src/test/java/eu/lundegaard/liferay/db/setup/CompanyIdTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) Lundegaard a.s. 2023 - All Rights Reserved
+ * Proprietary and confidential. Unauthorized copying of this file, via any
+ * medium is strictly prohibited.
+ */
+
+package eu.lundegaard.liferay.db.setup;
+
+/**
+ * @author Jakub Jandák, jakub.jandak@lundegaard.eu, 2023
+ */
+
+public class CompanyIdTest {
+
+}
